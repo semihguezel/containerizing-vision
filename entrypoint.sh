b475301b7d2e1ef682812167740c8e53c5641cd5
Semih Güzel <71209213+semihguezel@users.noreply.github.com>
2024-01-24 14:40:24 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Set the '-e' option to exit immediately if any command exits with a non-zero status.
 set -e
+
+# Add the '/src/vision_app/' directory to the Python module search path.
 export PYTHONPATH=$PYTHONPATH:'/src/vision_app/'
 
-python yolo_detector.py
+# Run the Python script 'yolo_detector.py' using Python 3.
+python3 yolo_detector.py
 
+# Execute any additional commands or scripts passed as arguments to this bash script.
 exec "$@"
